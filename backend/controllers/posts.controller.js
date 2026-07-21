ba808d08a8c034ca2bbabeb1567b8a87b5df522c
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-22 04:05:21 +0530

    configuring_server_routes_posts_models

---
+++ backend/controllers/posts.controller.js
@@ -0,0 +1,3 @@
+export const activeCheck = (req, res) => {
+    return res.status(200).json({message: "Running"});
+};
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express"; 
+import mongoose from "mongoose";
+import cors from "cors"; 
+import dotenv from "dotenv"; 
+import postRoutes from "./routes/posts.routes";
+
+dotenv.config();
+const app = express(); 
+app.use(cors());
+app.use(express.json());
+app.use(postRoutes);
+
+const PORT = process.env.PORT || 3000; 
+const MONGOOSE_URI = process.env.MONGOOSE_URI || ""; 
+
+const startServer = async () => {
+    await mongoose.connect(MONGOOSE_URI);
+
+    app.listen(3000, () => {
+        console.log(`Server is running on port: ${PORT}`);
+    });
+};
+
+startServer();
