62c583eebaf5ca9fd452d7775fd995cc25272a3c
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-22 20:44:34 +0530

    configured_loginUser_user_routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model";
 import profileModel from "../models/profile.model";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 export const registerUser = async (req, res) => {
   try {
@@ -31,6 +32,37 @@ export const registerUser = async (req, res) => {
     await user_profile.save();
 
     return res.status(200).json({ message: "user registered successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const {email, password} = req.body; 
+
+    if(!email || !password) {
+        return res.status(400).json({error: "All fields are required"});
+    };
+
+    const user = userModel.findOne({email});
+
+    if(!user){
+        return res.status(404).json({error: "User not found"});
+    }; 
+
+    const is_password_match = await bcrypt.compare(password, user.password);
+    
+    if(!is_password_match){
+        return res.status(400).json({error: "Invalid credentials"});
+    };
+
+    const token = crypto.randomBytes(32).toString("hex");
+    // user.token = token;    
+    // await user.save();
+    await userModel.updateOne({_id: user._id}, {token});
+
+    return res.status(200).json({message: "Login Successfull", token});
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.route("/auth/register-user").post(registerUser);
+router.route("/auth/login-user").post(loginUser);
 
 export default router;
