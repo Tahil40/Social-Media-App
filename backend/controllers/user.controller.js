af527c4dab9c274340188104f2530bd0742133c1
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-23 02:25:51 +0530

    configuring_multer_uploadProfilePicture_functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -39,30 +39,48 @@ export const registerUser = async (req, res) => {
 
 export const loginUser = async (req, res) => {
   try {
-    const {email, password} = req.body; 
+    const { email, password } = req.body;
 
-    if(!email || !password) {
-        return res.status(400).json({error: "All fields are required"});
-    };
+    if (!email || !password) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
 
-    const user = userModel.findOne({email});
+    const user = userModel.findOne({ email });
 
-    if(!user){
-        return res.status(404).json({error: "User not found"});
-    }; 
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
     const is_password_match = await bcrypt.compare(password, user.password);
-    
-    if(!is_password_match){
-        return res.status(400).json({error: "Invalid credentials"});
-    };
+
+    if (!is_password_match) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
 
     const token = crypto.randomBytes(32).toString("hex");
-    // user.token = token;    
+    // user.token = token;
     // await user.save();
-    await userModel.updateOne({_id: user._id}, {token});
+    await userModel.updateOne({ _id: user._id }, { token });
 
-    return res.status(200).json({message: "Login Successfull", token});
+    return res.status(200).json({ message: "Login Successfull", token });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const uploadProfilePicture = async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = await userModel.findOne({token: token});
+
+    if(!user){
+      return res.status(500).json({error: "user not found"});
+    };
+
+    user.profilePicture = req.file.filename;
+    await user.save();
+
+    return res.status(200).json({message: "Profile Picture Updated"});
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, uploadProfilePicture } from "../controllers/user.controller.js";
+import multer from "multer";
 
 const router = Router();
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({storage: storage});
+
 router.route("/auth/register-user").post(registerUser);
 router.route("/auth/login-user").post(loginUser);
+router.route("/update-profile-picture").post(upload.single("profile-picture"), uploadProfilePicture);
 
 export default router;
