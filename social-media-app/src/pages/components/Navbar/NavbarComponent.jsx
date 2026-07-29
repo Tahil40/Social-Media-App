933584c23eced3b9b5e68c7433bf112f588dc39b
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-29 21:23:01 +0530

    configured_landing_page_navbar_styling

--- social-media-app/src/pages/components/Navbar/NavbarComponent.jsx
+++ social-media-app/src/pages/components/Navbar/NavbarComponent.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import styles from "./navbar.module.css";
+import { useRouter } from "next/router";
 
 const NavbarComponent = () => {
+    const router = useRouter(); 
+
     return (
         <>
-        
+        <div className={styles.container}>
+            <nav className={styles.navbar}>
+                <h1 className={styles.heading1} onClick={()=>{router.push("/")}}>Pro Connect</h1>
+                <div className={styles.navbarOptionsContainer}>
+                    <div className={styles.joinButton} onClick={()=>{router.push("/login")}}>
+                        <p>Be a part</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
         </>
     );
 };
