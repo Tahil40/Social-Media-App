0f382e901af65291a0e852b55b60415f529e98de
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-08-01 13:41:14 +0530

    configured_show_messages_functionality_and_cardContainer_left_completed

--- social-media-app/src/pages/auth/login/index.js
+++ social-media-app/src/pages/auth/login/index.js
@@ -3,13 +3,16 @@ import UserLayout from "@/layouts/userLayout";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import style from "./auth.module.css";
-import { loginUser, registerUser } from "@/config/redux/action/authAction/AuthAction";
+import {
+  loginUser,
+  registerUser,
+} from "@/config/redux/action/authAction/AuthAction";
 
 export default function Login() {
   const [Name, SetName] = useState("");
-  const [UserName, SetUserName] = useState(""); 
-  const [Email, SetEmail] = useState(""); 
-  const [Password, SetPassword] = useState(""); 
+  const [UserName, SetUserName] = useState("");
+  const [Email, SetEmail] = useState("");
+  const [Password, SetPassword] = useState("");
   const router = useRouter();
   const auth_state = useSelector((state) => {
     state.auth;
@@ -20,16 +23,23 @@ export default function Login() {
     if (auth_state?.loggedIn) {
       router.push("/dashboard");
     }
-  }, [auth_state.loggedIn]);
+  }, [auth_state?.loggedIn]);
 
   const [IsSignInMethod, SetIsSignInMethod] = useState(false);
 
   const handleLogin = () => {
-    dispatch(loginUser({email: Email, password: Password}));
+    dispatch(loginUser({ email: Email, password: Password }));
   };
 
   const handleSignUp = () => {
-    dispatch(registerUser({name: Name, username: UserName, email: Email, password: Password}));
+    dispatch(
+      registerUser({
+        name: Name,
+        username: UserName,
+        email: Email,
+        password: Password,
+      }),
+    );
   };
 
   const handleSubmit = () => {
@@ -48,37 +58,54 @@ export default function Login() {
             <p className={style.cardContainer_left_heading}>
               {IsSignInMethod ? "Sign In" : "Sign up"}
             </p>
+            <p
+              className={auth_state?.isError ? style.red : style.green}
+              style={{ marginTop: "0.5rem" }}
+            >
+              {auth_state?.message.message}
+            </p>
+
             <div className={style.inputFormContainer}>
-              <div className={style.innerRow1}>
-                <input
-                  className={style.inputUserNameField}
-                  type="text"
-                  placeholder="Username"
-                  value={UserName}
-                  onChange={(e) => {SetUserName(e.target.value)}}
-                />
-                <input
-                  className={style.inputNameField}
-                  type="text"
-                  placeholder="Name"
-                  value={Name}
-                  onChange={(e) => {SetName(e.target.value)}}
-                />
-              </div>
+              {!IsSignInMethod && (
+                <div className={style.innerRow1}>
+                  <input
+                    className={style.inputUserNameField}
+                    type="text"
+                    placeholder="Username"
+                    value={UserName}
+                    onChange={(e) => {
+                      SetUserName(e.target.value);
+                    }}
+                  />
+                  <input
+                    className={style.inputNameField}
+                    type="text"
+                    placeholder="Name"
+                    value={Name}
+                    onChange={(e) => {
+                      SetName(e.target.value);
+                    }}
+                  />
+                </div>
+              )}
               <div className={style.innerRow2}>
                 <input
                   className={style.inputEmailField}
                   type="email"
                   placeholder="Email"
                   value={Email}
-                  onChange={(e) => {SetEmail(e.target.value)}}
+                  onChange={(e) => {
+                    SetEmail(e.target.value);
+                  }}
                 />
                 <input
                   className={style.inputPasswordField}
                   type="password"
                   placeholder="Password"
                   value={Password}
-                  onChange={(e) => {SetPassword(e.target.value)}}
+                  onChange={(e) => {
+                    SetPassword(e.target.value);
+                  }}
                 />
                 <button
                   className={style.inputSubmitField}
@@ -89,7 +116,9 @@ export default function Login() {
               </div>
             </div>
           </div>
-          <div className={style.cardContainer_right}></div>
+          <div className={style.cardContainer_right}>
+            
+          </div>
         </div>
       </div>
     </UserLayout>
